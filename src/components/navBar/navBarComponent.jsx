345933c3f2c5c6aed3837a320cfcca0d2b0d2423
unknown <gabrieltorresmora2@gmail.com>
2023-05-11 23:49:54 -0300

    Finalizando el Navbar falta el slider por finalizar y teniendo responsive en 4 secciones de las paginas

--- src/components/navBar/navBarComponent.jsx
+++ src/components/navBar/navBarComponent.jsx
@@ -1,27 +1,35 @@
+import { useState } from "react";
 import "./navBarComponent.css";
 
 export const NavBarComponent = () => {
-    return(
-        <div className="contenedorNavBar">
-            <div className="contenidoNavbar">
-                <img className="logoNavBar" src="img/RealLogo.png" alt="" />
-                <div className="redesSocialesNavBar">
-                        <a href="https://www.instagram.com/iamgabo22/"><img className="svgRedesSocialesNavbar" src="img/instagramWhite.svg" alt="" /></a>
-                        <a href="https://es-la.facebook.com/"><img className="svgRedesSocialesNavbar" src="img/facebookWhite.svg" alt="" /></a>
-                        <a href="https://github.com/GabrielDev22"><img className="svgRedesSocialesNavbar" src="img/githubWhite.svg" alt="" /></a>
-                        <a href="https://www.linkedin.com/in/gabriel-torres-431690229/"><img className="svgRedesSocialesNavbar" src="img/linkedinWhite.svg" alt="" /></a>
-                    </div>
 
-            <div className="linksPaginaNavBar">
-                <ul className="listadoNavbar">
-                    <li className="linksPaginasNavbar">Inicio</li>
-                    <li className="linksPaginasNavbar">Propiedades</li>
-                    <li className="linksPaginasNavbar">Noticias</li>
-                    <li className="linksPaginasNavbar">Equipo</li>
-                    <li className="linksPaginasNavbar">Contacto</li>
-                </ul>
+    const [isOpen, setIsOpen] = useState(false);
+
+
+    return(
+        <div className="navbar">
+            <div className="nav_logo"><span style={{color: "#E50E0B"}}>D</span>evGabriel</div>
+            <div className={`nav_items ${isOpen && "open"}` }>
+                <a href="#">Inicio</a>
+                <a href="#">Propiedades</a>
+                <a href="#">Noticias</a>
+                <a href="#">Equipo</a>
+                <a href="#">Contacto</a>
+            <div className="redesSociales"> 
+                <a href="https://www.instagram.com/iamgabo22/"><img src="img/instagramWhite.svg" alt="" /></a>
+                <a href="https://es-la.facebook.com/"><img src="img/facebookWhite.svg" alt="" /></a>
+                <a href="https://www.linkedin.com/in/gabriel-torres-431690229/"><img src="img/linkedinWhite.svg" alt="" /></a>
+                <a href="https://github.com/GabrielDev22"><img src="img/githubWhite.svg" alt="" /></a>
+            </div>    
             </div>
-            </div>
+
+        <div className={`nav_toggle ${isOpen && "open"}`} onClick={() => setIsOpen(!isOpen)}>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+
+
         </div>
     )
 }
